a741bde3e252a029b210a5ff35d8ba358a4f3b90
Dragos Tudor <dragos.tudor@gmail.com>
2023-12-09 18:33:05 +0000

    chore: use devcontainer features.

---
+++ certificates.sh
@@ -0,0 +1,7 @@
+set -e
+
+mkcert
+echo "export ROOT_CERT=$(mkcert -CAROOT)/rootCA.pem" >> $HOME/.bashrc
+
+[ ! -d "./.certificates" ] && mkdir ./.certificates
+[ ! -f "./.certificates/localhost.pem" ] && cd ./.certificates && mkcert localhost
