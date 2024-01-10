6fab175fada2d4cb58fda166b1b3e0e09d87943d
Dragos Tudor <dragos.tudor@gmail.com>
2024-01-10 10:59:07 +0000

    refactor: rename some authentication cookies tests.

--- Security.Authentication.Cookies/AuthenticationServices/Authenticating.Tests.cs
+++ Security.Authentication.Cookies/AuthenticationServices/Authenticating.Tests.cs
@@ -4,64 +4,65 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Authentication;
 using static Security.Testing.Funcs;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 namespace Security.Authentication.Cookies;
 
 partial class CookiesTests
 {
   [Fact]
-  public async Task Authenticated_user__access_protected_resource__access_allowed()
+  public async Task Authenticated_user_with_cookie__authenticate__authenticated_user()
   {
     using var server = CreateHttpServer(services => services.AddCookies(CreateCookieAuthenticationOptions()));
     server.UseAuthentication(AuthenticateCookie);
-    server.MapPost("/account/login", (HttpContext context) => SignInCookie(context, CreateNamedClaimsPrincipal("Cookies", "user")).ToString());
+    server.MapPost("/account/signin", (HttpContext context) => SignInCookie(context, CreateNamedClaimsPrincipal("user")).ToString());
     server.MapGet("/resource", (HttpContext context) => GetPrincipalName(context.User) );
     await server.StartAsync();
 
     using var client = server.GetTestClient();
-    using var loginResponse = await client.PostAsync("/account/login");
-    using var response = await client.GetAsync("/resource", GetRequestMessageCookieHeader(loginResponse));
+    using var signinResponse = await client.PostAsync("/account/signin");
+    using var response = await client.GetAsync("/resource", GetRequestMessageCookieHeader(signinResponse));
 
     Assert.Equal("user", await ReadResponseMessageContent(response));
   }
 
   [Fact]
-  public async Task Authenticated_user_with_session_based_authentication_cookie__access_protected_resource__access_allowed()
+  public async Task Authenticated_user_with_session_based_authentication_cookie__authenticate__authenticated_user()
   {
     using var server = CreateHttpServer(services => services.AddCookies(CreateCookieAuthenticationOptions(), new FakeTicketStore()));
     server.UseAuthentication(AuthenticateCookie);
-    server.MapPost("/account/login", (HttpContext context) => SignInCookie(context, CreateNamedClaimsPrincipal("Cookies", "user")).ToString());
+    server.MapPost("/account/signin", (HttpContext context) => SignInCookie(context, CreateNamedClaimsPrincipal("user")).ToString());
     server.MapGet("/resource", (HttpContext context) => GetPrincipalName(context.User) );
     await server.StartAsync();
 
     using var client = server.GetTestClient();
-    using var loginResponse = await client.PostAsync("/account/login");
-    using var response = await client.GetAsync("/resource", GetRequestMessageCookieHeader(loginResponse));
+    using var signinResponse = await client.PostAsync("/account/signin");
+    using var response = await client.GetAsync("/resource", GetRequestMessageCookieHeader(signinResponse));
 
     Assert.Equal("user", await ReadResponseMessageContent(response));
   }
 
   [Fact]
-  public async Task Authenticated_user__access_protected_resource__access_allowed_microsoft()
+  public async Task Authenticated_user_with_cookie__authenticate__authenticated_user_microsoft()
   {
     using var server = CreateHttpServer(services => services.AddAuthentication().AddCookie());
     server.UseAuthentication();
-    server.MapPost("/account/login", (HttpContext context) => context.SignInAsync(CreatePrincipal("Cookies", new [] { CreateNameClaim("user") } )) );
+    server.MapPost("/account/signin", (HttpContext context) => context.SignInAsync(CreatePrincipal("Cookies", new [] { CreateNameClaim("user") } )) );
     server.MapGet("/resource", (HttpContext context) => GetPrincipalName(context.User) );
     await server.StartAsync();
 
     using var client = server.GetTestClient();
-    using var loginResponse = await client.PostAsync("/account/login", default);
-    using var response = await client.GetAsync("/resource", GetRequestMessageCookieHeader(loginResponse));
+    using var signinResponse = await client.PostAsync("/account/signin", default);
+    using var response = await client.GetAsync("/resource", GetRequestMessageCookieHeader(signinResponse));
 
     Assert.Equal("user", await ReadResponseMessageContent(response));
   }
 
   [Fact]
-  public async Task Authenticated_user_by_identity_app__access_other_api_resource__user_authenticated()
+  public async Task Authenticated_user_by_some_api__access_other_api_resource__authenticated_user()
   {
     using var identityServer = CreateHttpServer(services => services.AddCookies());
-    identityServer.MapPost("/account/login", (HttpContext context) => SignInCookie(context, CreateNamedClaimsPrincipal("Cookies", "user")).ToString() );
+    identityServer.MapPost("/account/signin", (HttpContext context) => SignInCookie(context, CreateNamedClaimsPrincipal("user")).ToString() );
     await identityServer.StartAsync();
 
     using var apiServer = CreateHttpServer(services => services.AddCookies());
@@ -70,19 +71,19 @@ partial class CookiesTests
     await apiServer.StartAsync();
 
     using var identityClient = identityServer.GetTestClient();
-    using var loginResponse = await identityClient.PostAsync("/account/login");
+    using var signinResponse = await identityClient.PostAsync("/account/signin");
 
     using var apiClient = apiServer.GetTestClient();
-    using var apiResponse = await apiClient.GetAsync("/resource", GetRequestMessageCookieHeader(loginResponse));
+    using var apiResponse = await apiClient.GetAsync("/resource", GetRequestMessageCookieHeader(signinResponse));
 
     Assert.Equal("user", await ReadResponseMessageContent(apiResponse));
   }
 
   [Fact]
-  public async Task Authenticated_user_by_identity_app__access_other_api_resource__user_authenticated_microsoft()
+  public async Task Authenticated_user_by_some_api__access_other_api_resource__authenticated_user_microsoft()
   {
     using var identityServer = CreateHttpServer(services => services.AddAuthentication().AddCookie());
-    identityServer.MapPost("/account/login", (HttpContext context) => context.SignInAsync(CreatePrincipal("Cookies", new [] { CreateNameClaim("user") })) );
+    identityServer.MapPost("/account/signin", (HttpContext context) => context.SignInAsync(CreatePrincipal("Cookies", new [] { CreateNameClaim("user") })) );
     await identityServer.StartAsync();
 
     using var apiServer = CreateHttpServer(services => services.AddAuthentication().AddCookie());
@@ -91,54 +92,54 @@ partial class CookiesTests
     await apiServer.StartAsync();
 
     using var identityClient = identityServer.GetTestClient();
-    using var loginResponse = await identityClient.PostAsync("/account/login", default);
+    using var signinResponse = await identityClient.PostAsync("/account/signin", default);
 
     using var apiClient = apiServer.GetTestClient();
-    using var apiResponse = await apiClient.GetAsync("/resource", GetRequestMessageCookieHeader(loginResponse));
+    using var apiResponse = await apiClient.GetAsync("/resource", GetRequestMessageCookieHeader(signinResponse));
 
     Assert.Equal("user", await ReadResponseMessageContent(apiResponse));
   }
 
   [Fact]
-  public async Task Authenticated_user_wuth_expired_authentication_cookie__access_protected_resource__unauthenticated_user()
+  public async Task Authenticated_user_wuth_expired_authentication_cookie__authenticate__unauthenticated_user()
   {
     var expireCookieTicket = TimeSpan.FromMinutes(10);
     using var server = CreateHttpServer(services => services.AddCookies((CreateCookieAuthenticationOptions()) with { ExpireTimeSpan = expireCookieTicket }));
     server.UseAuthentication(AuthenticateCookie);
-    server.MapPost("/account/login", (HttpContext context) => SignInCookie(context, CreateNamedClaimsPrincipal("Cookies", "user")).ToString());
+    server.MapPost("/account/signin", (HttpContext context) => SignInCookie(context, CreateNamedClaimsPrincipal("user")).ToString());
     server.MapGet("/resource", (HttpContext context) => GetPrincipalIdentity(context.User)!.IsAuthenticated ? "auth" : "unauth");
     await server.StartAsync();
 
     using var client = server.GetTestClient();
-    using var loginResponse = await client.PostAsync("/account/login");
+    using var signinResponse = await client.PostAsync("/account/signin");
     GetFakeTimeProvider(server.Services).Advance(TimeSpan.FromMinutes(11));
-    using var response = await client.GetAsync("/resource", GetRequestMessageCookieHeader(loginResponse));
+    using var response = await client.GetAsync("/resource", GetRequestMessageCookieHeader(signinResponse));
 
     Assert.Equal("unauth", await ReadResponseMessageContent(response));
   }
 
   [Fact]
-  public async Task Authenticated_user_wuth_expired_session_based_authentication_cookie__access_protected_resource__unauthenticated_user()
+  public async Task Authenticated_user_wuth_expired_session_based_authentication_cookie__authenticate__unauthenticated_user()
   {
     var expireCookieTicket = TimeSpan.FromMinutes(10);
     using var server = CreateHttpServer(services => services.AddCookies(
       CreateCookieAuthenticationOptions() with { ExpireTimeSpan = expireCookieTicket },
       new FakeTicketStore()));
     server.UseAuthentication(AuthenticateCookie);
-    server.MapPost("/account/login", (HttpContext context) => SignInCookie(context, CreateNamedClaimsPrincipal("Cookies", "user")).ToString());
+    server.MapPost("/account/signin", (HttpContext context) => SignInCookie(context, CreateNamedClaimsPrincipal("user")).ToString());
     server.MapGet("/resource", (HttpContext context) => GetPrincipalIdentity(context.User)!.IsAuthenticated ? "auth" : "unauth");
     await server.StartAsync();
 
     using var client = server.GetTestClient();
-    using var loginResponse = await client.PostAsync("/account/login");
+    using var signinResponse = await client.PostAsync("/account/signin");
     GetFakeTimeProvider(server.Services).Advance(TimeSpan.FromMinutes(11));
-    using var response = await client.GetAsync("/resource", GetRequestMessageCookieHeader(loginResponse));
+    using var response = await client.GetAsync("/resource", GetRequestMessageCookieHeader(signinResponse));
 
     Assert.Equal("unauth", await ReadResponseMessageContent(response));
   }
 
   [Fact]
-  public async Task Authenticated_user_wuth_expired_session_based_authentication_cookie__access_protected_resource__expired_cookie_and_removed_session_ticket_from_store()
+  public async Task Authenticated_user_wuth_expired_session_based_authentication_cookie__authenticate__expired_cookie_and_removed_session_ticket_from_store()
   {
     var ticketStore = new FakeTicketStore();
     var expireCookieTicket = TimeSpan.FromMinutes(10);
@@ -146,15 +147,15 @@ partial class CookiesTests
       CreateCookieAuthenticationOptions() with { ExpireTimeSpan = expireCookieTicket },
       ticketStore));
     server.UseAuthentication(AuthenticateCookie);
-    server.MapPost("/account/login", (HttpContext context) => SignInCookie(context, CreateNamedClaimsPrincipal("Cookies", "user")).ToString());
+    server.MapPost("/account/signin", (HttpContext context) => SignInCookie(context, CreateNamedClaimsPrincipal("user")).ToString());
     server.MapGet("/resource", (HttpContext context) => GetPrincipalIdentity(context.User)!.IsAuthenticated ? "auth" : "unauth");
     await server.StartAsync();
 
     using var client = server.GetTestClient();
-    using var loginResponse = await client.PostAsync("/account/login");
+    using var signinResponse = await client.PostAsync("/account/signin");
     GetFakeTimeProvider(server.Services).Advance(TimeSpan.FromMinutes(11));
-    var ticketId = GetSessionBasedCookieTicketId(loginResponse, server.Services);
-    using var response = await client.GetAsync("/resource", GetRequestMessageCookieHeader(loginResponse));
+    var ticketId = GetSessionBasedCookieTicketId(signinResponse, server.Services);
+    using var response = await client.GetAsync("/resource", GetRequestMessageCookieHeader(signinResponse));
 
     Assert.True(response.IsSuccessStatusCode);
     Assert.Contains(".AspNetCore.Cookies=;", GetResponseMessageCookie(response));
@@ -163,70 +164,70 @@ partial class CookiesTests
   }
 
   [Fact]
-  public async Task Authenticated_user_wuth_renewable_session_based_authentication_cookie__access_protected_resource__authenticated_user()
+  public async Task Authenticated_user_wuth_renewable_session_based_authentication_cookie__authenticate__authenticated_user()
   {
     var expireCookieTicket = TimeSpan.FromMinutes(10);
     using var server = CreateHttpServer(services => services.AddCookies(
       CreateCookieAuthenticationOptions() with { ExpireTimeSpan = expireCookieTicket },
       new FakeTicketStore()));
     server.UseAuthentication(AuthenticateCookie);
-    server.MapPost("/account/login", (HttpContext context) => SignInCookie(context, CreateNamedClaimsPrincipal("Cookies", "user")).ToString());
+    server.MapPost("/account/signin", (HttpContext context) => SignInCookie(context, CreateNamedClaimsPrincipal("user")).ToString());
     server.MapGet("/resource", (HttpContext context) => GetPrincipalIdentity(context.User)!.IsAuthenticated ? "auth" : "unauth");
     await server.StartAsync();
 
     using var client = server.GetTestClient();
-    using var loginResponse = await client.PostAsync("/account/login");
+    using var signinResponse = await client.PostAsync("/account/signin");
     GetFakeTimeProvider(server.Services).Advance(TimeSpan.FromMinutes(7));
-    using var response = await client.GetAsync("/resource", GetRequestMessageCookieHeader(loginResponse));
+    using var response = await client.GetAsync("/resource", GetRequestMessageCookieHeader(signinResponse));
 
     Assert.Equal("auth", await ReadResponseMessageContent(response));
   }
 
   [Fact]
-  public async Task Authenticated_user_wuth_renewable_session_based_authentication_cookie__access_protected_resource__renewed_authentication_cookie()
+  public async Task Authenticated_user_wuth_renewable_session_based_authentication_cookie__authenticate__renewed_authentication_cookie()
   {
     var expireCookieTicket = TimeSpan.FromMinutes(10);
     using var server = CreateHttpServer(services => services.AddCookies(
       CreateCookieAuthenticationOptions() with { ExpireTimeSpan = expireCookieTicket },
       new FakeTicketStore()));
     server.UseAuthentication(AuthenticateCookie);
-    server.MapPost("/account/login", (HttpContext context) => SignInCookie(context, CreateNamedClaimsPrincipal("Cookies", "user")).ToString());
+    server.MapPost("/account/signin", (HttpContext context) => SignInCookie(context, CreateNamedClaimsPrincipal("user")).ToString());
     server.MapGet("/resource", (HttpContext context) => GetPrincipalIdentity(context.User)!.IsAuthenticated ? "auth" : "unauth");
     await server.StartAsync();
 
     using var client = server.GetTestClient();
-    using var loginResponse = await client.PostAsync("/account/login");
-    var initialCookie = GetResponseMessageCookie(loginResponse);
+    using var signinResponse = await client.PostAsync("/account/signin");
+    var initialCookie = GetResponseMessageCookie(signinResponse);
     GetFakeTimeProvider(server.Services).Advance(TimeSpan.FromMinutes(7));
-    using var response = await client.GetAsync("/resource", GetRequestMessageCookieHeader(loginResponse));
+    using var response = await client.GetAsync("/resource", GetRequestMessageCookieHeader(signinResponse));
     var renewedCookie = GetResponseMessageCookie(response);
 
     Assert.NotEqual(initialCookie, renewedCookie);
   }
 
   [Fact]
-  public async Task Authenticated_user_with_expired_authentication_cookie__access_protected_resource__unauthenticated_user_mictrosoft()
+  public async Task Authenticated_user_with_expired_authentication_cookie__authenticate__unauthenticated_user_mictrosoft()
   {
     var expireCookieTicket = TimeSpan.FromMinutes(10);
     using var server = CreateHttpServer(services => services.AddAuthentication().AddCookie(o => o.ExpireTimeSpan = expireCookieTicket));
     server.UseAuthentication();
-    server.MapPost("/account/login", (HttpContext context) => context.SignInAsync(CreatePrincipal("Cookies", new [] { CreateNameClaim("user") } )) );
+    server.MapPost("/account/signin", (HttpContext context) => context.SignInAsync(CreatePrincipal("Cookies", new [] { CreateNameClaim("user") } )) );
     server.MapGet("/resource", (HttpContext context) => GetPrincipalIdentity(context.User)!.IsAuthenticated ? "auth" : "unauth");
     await server.StartAsync();
 
     using var client = server.GetTestClient();
-    using var loginResponse = await client.PostAsync("/account/login", default);
+    using var signinResponse = await client.PostAsync("/account/signin", default);
     GetFakeTimeProvider(server.Services).Advance(TimeSpan.FromMinutes(11));
-    using var response = await client.GetAsync("/resource", GetRequestMessageCookieHeader(loginResponse));
+    using var response = await client.GetAsync("/resource", GetRequestMessageCookieHeader(signinResponse));
 
     Assert.Equal("unauth", await ReadResponseMessageContent(response));
   }
 
   [Fact]
-  public async Task Authenticated_user_by_identity_api__interop_access_other_api__authenticated_user()
+  public async Task Authenticated_user_by_identity_api__interop_authenticate__authenticated_user()
   {
     using var identityServer = CreateHttpServer(services => services.AddCookies());
-    identityServer.MapPost("/account/login", (HttpContext context) => SignInCookie(context, CreateNamedClaimsPrincipal("Cookies", "user")).ToString() );
+    identityServer.MapPost("/account/signin", (HttpContext context) => SignInCookie(context, CreateNamedClaimsPrincipal("user", "Cookies")).ToString() );
     await identityServer.StartAsync();
 
     using var apiServer = CreateHttpServer(services => services.AddAuthentication().AddCookie());
@@ -235,15 +236,15 @@ partial class CookiesTests
     await apiServer.StartAsync();
 
     using var identityClient = identityServer.GetTestClient();
-    using var loginResponse = await identityClient.PostAsync("/account/login");
+    using var signinResponse = await identityClient.PostAsync("/account/signin");
 
     using var apiClient = apiServer.GetTestClient();
-    using var apiResponse = await apiClient.GetAsync("/resource", GetRequestMessageCookieHeader(loginResponse));
+    using var apiResponse = await apiClient.GetAsync("/resource", GetRequestMessageCookieHeader(signinResponse));
 
     Assert.Equal("user", await ReadResponseMessageContent(apiResponse));
   }
 
-  static ClaimsPrincipal CreateNamedClaimsPrincipal (string schemeName, string name) =>
+  static ClaimsPrincipal CreateNamedClaimsPrincipal (string name, string schemeName = CookieAuthenticationDefaults.AuthenticationScheme) =>
     CreatePrincipal(schemeName, new [] { CreateNameClaim(name) });
 
 }
