b1cb1a2db99a0decb1b97ba3822fdec612301ded
Dragos Tudor <dragos.tudor@gmail.com>
2024-02-14 10:20:50 +0000

    fix: register missing oidc service.

--- Security.Authentication.OpenIdConnect/ServiceCollections/Adding.cs
+++ Security.Authentication.OpenIdConnect/ServiceCollections/Adding.cs
@@ -2,6 +2,7 @@
 using System.Net.Http;
 using Microsoft.AspNetCore.DataProtection;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
 
 namespace Security.Authentication.OpenIdConnect;
 
@@ -10,12 +11,14 @@ partial class OpenIdConnectFuncs
   public static IServiceCollection AddOpenIdConnect<TOptions>(
     this IServiceCollection services,
     TOptions authOptions,
+    OpenIdConnectConfiguration oidcConfiguration,
     HttpClient? httpClient = default,
     IDataProtectionProvider? dataProtectionProvider = default,
     TimeProvider? timeProvider = default)
   where TOptions : OpenIdConnectOptions =>
       services
         .AddSingleton(authOptions)
+        .AddSingleton(oidcConfiguration)
         .AddSingleton(services =>
           CreateOpenIdConnectDeps(services, authOptions, httpClient, dataProtectionProvider, timeProvider));
 }
