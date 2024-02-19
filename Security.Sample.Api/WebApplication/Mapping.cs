0aae8536b1396b37dc3c66c3e65060fd700133d0
Dragos Tudor <dragos.tudor@gmail.com>
2024-02-19 10:21:30 +0000

    feat: set sample api signout authorization.

--- Security.Sample.Api/WebApplication/Mapping.cs
+++ Security.Sample.Api/WebApplication/Mapping.cs
@@ -6,7 +6,7 @@ partial class SampleFuncs
   static WebApplication MapEndpoints (WebApplication app)
   {
     app.MapPost("/api/accounts/signin", (Delegate)SignInEndpoint);
-    app.MapPost("/api/accounts/signout", SignOutEndpoint);
+    app.MapPost("/api/accounts/signout", SignOutEndpoint).RequireAuthorization();
     app.MapFacebook(ResolveService<FacebookOptions>(app.Services), SignInCookie);
     app.MapGoogle(ResolveService<GoogleOptions>(app.Services), SignInCookie);
     app.MapTwitter(ResolveService<TwitterOptions>(app.Services), SignInCookie);
