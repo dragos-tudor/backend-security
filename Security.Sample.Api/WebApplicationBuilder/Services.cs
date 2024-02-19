5c673aa5499064ccb1b35d0f9a287b5004a47e0b
Dragos Tudor <dragos.tudor@gmail.com>
2024-02-19 08:58:59 +0000

    featL register remote clients for each oauth providers.

--- Security.Sample.Api/WebApplicationBuilder/Services.cs
+++ Security.Sample.Api/WebApplicationBuilder/Services.cs
@@ -1,21 +1,21 @@
 using Microsoft.AspNetCore.DataProtection;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using static Security.Authentication.Remote.RemoteFuncs;
 
-namespace Security.Samples;
+namespace Security.Sample.Api;
 
-partial class SampleFuncs {
-
-  internal static IServiceCollection AddServices(WebApplicationBuilder app, string keysPath) =>
+partial class SampleFuncs
+{
+  static IServiceCollection AddServices(WebApplicationBuilder app, string keysPath) =>
     app.Services
       .AddCookies(SetCookieOptions())
-      .AddGoogle(SetGoogleOptions(app))
-      .AddFacebook(SetFacebookOptions(app))
-      .AddTwitter(SetTwitterOptions(app))
+      .AddGoogle(SetGoogleOptions(app), SetRemoteClient(CreateRemoteClient(), GoogleDefaults.AuthenticationScheme))
+      .AddFacebook(SetFacebookOptions(app), SetRemoteClient(CreateRemoteClient(), FacebookDefaults.AuthenticationScheme))
+      .AddTwitter(SetTwitterOptions(app), SetRemoteClient(CreateRemoteClient(), TwitterDefaults.AuthenticationScheme))
       .AddAuthorizationCore()
       .AddLogging(o => o.SetMinimumLevel(LogLevel.Warning))
       .AddDataProtection()
       .PersistKeysToFileSystem(new (keysPath))
       .Services;
-
 }
