167fb057457afce3128b3ff314a1d6be00e9b719
Dragos Tudor <dragos.tudor@gmail.com>
2024-02-27 12:38:42 +0000

    deps(www): update fetching usage patterns.

--- Security.Sample.www/scripts/fetching.js
+++ Security.Sample.www/scripts/fetching.js
@@ -1,54 +1,42 @@
-const addHeader = (headers, name, value)=>{
-    if (headers == null) headers = new Headers();
+const setJsonContentTypeHeader = (headers)=>setHeader(headers, "Content-Type", "application/json");
+const setHeader = (headers, name, value)=>{
     headers instanceof Headers ? headers.set(name, value) : headers[name] = value;
     return headers;
 };
-const toJsonBody = (method, obj = {})=>method !== "GET" && method !== "HEAD" ? JSON.stringify(obj) : null;
-const createJsonRequest = (method, request)=>({
-        ...request,
-        body: toJsonBody(method, request.body),
-        headers: addHeader(request.headers, "Content-Type", "application/json"),
-        method
-    });
-const createFetchError = async (response)=>Object.freeze({
-        name: "FetchError",
-        message: await response.text() || response.statusText,
-        status: response.status
-    });
-const createNetworkError = (error)=>Object.freeze({
-        name: "NetworkError",
-        message: error.message,
-        stack: error.stack
-    });
-const rejectAbortError = (err)=>Promise.reject(err);
-const rejectFetchError = async (response)=>Promise.reject(await createFetchError(response));
-const rejectNetworkError = (err)=>Promise.reject(createNetworkError(err));
-const getJsonResponse = (response)=>response.status !== 204 && response.body != null ? response.json() : Promise.resolve(null);
-const getFetchResponse = (response)=>response.ok ? response : rejectFetchError(response);
-const fetchData = (url, request, fetchFunc = fetch)=>fetchFunc(url, request).catch((error)=>error.name === "AbortError" ? rejectAbortError(error) : rejectNetworkError(error)).then(getFetchResponse);
-const fetchJson = (method)=>async (url, request, fetchFunc = fetch)=>{
-        const jsonRequest = createJsonRequest(method, request || {});
-        const response = await fetchData(url, jsonRequest, fetchFunc);
-        return getJsonResponse(response);
-    };
-const getJson = fetchJson("GET");
-const postJson = fetchJson("POST");
-const putJson = fetchJson("PUT");
-const deleteJson = fetchJson("DELETE");
-const headJson = fetchJson("HEAD");
-const patchJson = fetchJson("PATCH");
-export { fetchJson as fetchJson, fetchData as fetchData, getJson as getJson, postJson as postJson, putJson as putJson, deleteJson as deleteJson, headJson as headJson, patchJson as patchJson };
-const chainMiddlewares = (middlewares, fetchFunc = fetch)=>middlewares?.length ? middlewares.reverse().reduce((acc, next)=>next(acc), fetchFunc) : fetchFunc;
+const HttpMethods = Object.freeze({
+    GET: "GET",
+    POST: "POST",
+    PUT: "PUT",
+    PATCH: "PATCH",
+    DELETE: "DELETE",
+    OPTIONS: "OPTIONS",
+    HEAD: "HEAD"
+});
+const isGetMethod = (method)=>method === HttpMethods.GET;
+const isHeadMethod = (method)=>method === HttpMethods.HEAD;
+const toJsonRequestBody = (obj)=>obj ? JSON.stringify(obj) : null;
+const ensureRequestHeaders = (request)=>request.headers = request.headers ?? new Headers();
+const ensureRequestMethod = (request, method)=>request.method = request.method ?? method;
+const setRequestBody = (request, body)=>body ? Object.assign(request, {
+        body
+    }) : body;
+const allowRequestBody = (method)=>!(isGetMethod(method) || isHeadMethod(method));
+const allowRequestSearchParams = (method)=>isGetMethod(method) || isHeadMethod(method);
+const existsResponseContent = (response)=>response.status !== 204 && response.body != null;
+const isResponseOk = (response)=>response.ok;
+const isResponseError = (response)=>response instanceof Error;
+const getTextResponse = async (response)=>await response.text() || response.statusText;
+const getJsonResponse = (response)=>existsResponseContent(response) ? response.json() : Promise.resolve(null);
 const getSearchParam = (field, value)=>`${encodeURIComponent(field)}=${encodeURIComponent(value)}`;
 const getSearchParams = (obj)=>(propName)=>{
         const propValue = obj[propName];
-        const isArray = propValue instanceof Array;
         switch(propValue){
             case null:
                 return propName;
             case undefined:
                 return undefined;
         }
+        const isArray = propValue instanceof Array;
         switch(isArray){
             case false:
                 return getSearchParam(propName, propValue);
@@ -57,29 +45,64 @@ const getSearchParams = (obj)=>(propName)=>{
         }
     };
 const toStringSearchParams = (obj)=>Object.getOwnPropertyNames(obj).map(getSearchParams(obj)).filter((params)=>!!params).join("&");
-const encodeSearchParams = (url, obj = {})=>{
-    const searchParams = toStringSearchParams(obj);
-    return `${encodeURI(url)}${searchParams ? "?" : ""}${searchParams}`;
+const encodeSearchParams = (url, obj)=>{
+    const searchParams = obj ? toStringSearchParams(obj) : undefined;
+    return searchParams ? `${url}?${searchParams}` : url;
 };
-const absoluteUrlRegex = /^http(s)*:\/\//i;
-const toAbsoluteUrl = (url, baseUrl = "")=>absoluteUrlRegex.test(url) ? url : `${baseUrl}${url}`;
 export { encodeSearchParams as encodeSearchParams };
-export { toAbsoluteUrl as toAbsoluteUrl };
-const retry = async (numberOf, func, ...args)=>{
-    for(let retries = 0; retries <= numberOf; retries++)try {
-        return await func(...args);
-    } catch (error) {
-        if (retries === numberOf) return Promise.reject(error);
-    }
-    return Promise.reject("Max retries reached.");
-};
-const setTokenHeader = (request, token)=>Object.assign(request, {
-        headers: request.headers
-    }, {
-        headers: token
+const createAbortError = (error)=>Object.assign(Error(error.code), {
+        type: "AbortError"
     });
-const createBaseUrlMiddleware = (baseUrl)=>(next)=>(url, request)=>next(toAbsoluteUrl(url, baseUrl), request);
-const createRetryMiddleware = (retries)=>(next)=>(url, request)=>retry(retries, next, url, request);
-const createTokenMiddleware = (token)=>(next)=>(url, request)=>next(url, setTokenHeader(request, token));
-export { chainMiddlewares as chainMiddlewares };
-export { createBaseUrlMiddleware as createBaseUrlMiddleware, createRetryMiddleware as createRetryMiddleware, createTokenMiddleware as createTokenMiddleware };
+const createHttpError = (response, message)=>Object.assign(Error(message), {
+        type: "HttpError",
+        status: response.status
+    });
+const createNetworkError = (error)=>Object.assign(Error(error.message), {
+        type: "NetworkError",
+        stack: error.stack
+    });
+const fetchData = async (fetch, url, request)=>{
+    try {
+        const response = await fetch(url, request);
+        return isResponseOk(response) ? response : createHttpError(response, await getTextResponse(response));
+    } catch (error) {
+        return error.name === "AbortError" ? createAbortError(error) : createNetworkError(error);
+    }
+};
+const fetchJson = async (fetch, url, data, request = {})=>{
+    ensureRequestMethod(request, HttpMethods.GET);
+    if (allowRequestBody(request.method)) {
+        ensureRequestHeaders(request, request.headers);
+        setJsonContentTypeHeader(request.headers);
+        setRequestBody(request, toJsonRequestBody(data));
+    }
+    const response = allowRequestSearchParams(request.method) ? await fetchData(fetch, encodeSearchParams(url, data), request) : await fetchData(fetch, url, request);
+    return !isResponseError(response) ? await getJsonResponse(response) : response;
+};
+export { fetchJson as fetchJson, fetchData as fetchData };
+const waitTimeout = (timeout)=>new Promise((resolve)=>{
+        const timeoutId = setTimeout(()=>{
+            clearTimeout(timeoutId);
+            resolve();
+        }, timeout);
+    });
+const isFirstIndex = (index)=>index === 0;
+const isLastIndex = (index, intervals)=>index === intervals.length - 1;
+const expBackoff = async (intervals, func, ...args)=>{
+    for (const [index, interval] of intervals.entries()){
+        if (!isFirstIndex(index)) await waitTimeout(interval);
+        const result = await func(...args);
+        if (!(result instanceof Error)) return result;
+        if (isLastIndex(index, intervals)) return result;
+    }
+};
+export { expBackoff as expBackoff };
+const retry = async (retries, func, ...args)=>{
+    do {
+        const result = await func(...args);
+        if (!(result instanceof Error)) return result;
+        if (!retries) return result;
+    }while (retries--)
+};
+export { retry as retry };
+export { HttpMethods as HttpMethods };
