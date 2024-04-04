bf05dfcee1975e1149df0ad483d3a1bbd0c33c8a
Dragos Tudor <dragos.tudor@gmail.com>
2024-04-04 15:16:28 +0000

    www: remove unused folder.

--- support/elements/finding.js
+++
@@ -1,7 +0,0 @@
-
-
-export const findAscendantElement = (elem, func) => {
-  if(func(elem)) return elem
-  if(!getParentElement(elem)) return undefined
-  return findAscendantElement(getParentElement(elem), func)
-}
