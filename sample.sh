fd3820dfe9e4fe5b385c97a24fbca239c6521588
Dragos Tudor <dragos.tudor@gmail.com>
2024-04-19 13:23:29 +0000

    change starting sample app script.

--- sample.sh
+++ sample.sh
@@ -1,8 +1,11 @@
+set -e
 trap 'kill 0' SIGINT;
+trap 'kill 0' SIGCHLD;
 
 SAMPLE_PATH=/workspaces/backend-security/Security.Sample
 SAMPLE_API_PATH=$SAMPLE_PATH.Api
 SAMPLE_WWW_PATH=$SAMPLE_PATH.www
 
-deno task start $SAMPLE_WWW_PATH & \
-dotnet run --no-restore --no-build --project $SAMPLE_API_PATH
+deno task start $SAMPLE_WWW_PATH &
+dotnet run --no-restore --no-build --project $SAMPLE_API_PATH &
+wait
